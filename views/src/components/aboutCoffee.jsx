41533b2c18400b55c412d9976452a30c0afa5a92
jasperayawan <ejayawan22@gmail.com>
2023-06-05 03:10:34 +0800

    update height

--- views/src/components/aboutCoffee.jsx
+++ views/src/components/aboutCoffee.jsx
@@ -1,11 +1,11 @@
 
 export default function AboutCoffee(){
     return(
-        <div className="grid grid-cols-1 md:grid-cols-2 min-h-screen">
-            <div className="imageCoffee_about"></div>
-            <div className="flex justify-center h-full">
+        <div className="grid grid-cols-1 md:grid-cols-2">
+            <div className="imageCoffee_about h-screen"></div>
+            <div className="flex justify-center h-screen">
                 <div className="max-w-md gap-4 flex flex-col px-5 justify-center h-full">
-                    <h1 className="text-3xl">Single Origin Coffee Imported World wide</h1>
+                    <h1 className="text-3xl font-bold text-stone-800">Single Origin Coffee Imported World wide</h1>
                     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
                         Maxime, voluptate molestias tenetur aliquid ipsa voluptas atque 
                         necessitatibus repudiandae! Est, commodi.</p>
